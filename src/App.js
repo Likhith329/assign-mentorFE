3100d6c26523c658f137212067d6cdc5ebc115bf
Likhith kumar <likhithmattapalli@gmail.com>
2022-11-23 17:51:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       <nav  className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#" onClick={()=>{navigate('/')}}>StudentMentorAssignings</a>
+          <a className="navbar-brand" href="#" onClick={()=>{navigate('/assign-mentorFE')}}>StudentMentorAssignings</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
